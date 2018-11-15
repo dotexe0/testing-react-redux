08de5e6d92ef4bbc2031855ab2038bc9633d61a8
Daniel Varela <dvarela@wiser.com>
2018-11-14 16:50:03 -0800

    cleaner commentbox test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,23 +17,21 @@ it('has a text area and a button', () => {
   expect(wrapped.find('button').length).toEqual(1)
 })
 
-it('has a text area that users can type in', () => {
-  wrapped.find('textarea').simulate('change', { 
-    target: { name: 'comment', value: 'new comment' } 
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { name: 'comment', value: 'new comment' }
+    })
+    wrapped.update() // simulate re-render
   })
-  wrapped.update() // simulate re-render
+it('has a text area that users can type in', () => {
   expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
 })
 
 it('submits the input and empties the textarea', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: {name: 'comment', value: 'i want to submit this'}
-  })
-  wrapped.update()
-  // check that textarea has been updated
-  expect(wrapped.find('textarea').prop('value')).toEqual('i want to submit this')
   wrapped.find('form').simulate('submit')
   wrapped.update()
-  // check that form is reset
+
   expect(wrapped.find('textarea').prop('value')).toEqual('')
+})
 })
