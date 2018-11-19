16d973078a5cda3bccad12d5f2e761989e1c8aa6
Daniel Varela <dvarela@wiser.com>
2018-11-19 09:44:14 -0800

    add Route to app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import { BrowserRouter, Route } from 'react-router-dom'
 import App from 'components/App'
 import Root from 'Root'
 
 const appWithReduxStore = 
   <Root>  
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
   </Root>
 
 ReactDOM.render(appWithReduxStore, document.getElementById('root'))
