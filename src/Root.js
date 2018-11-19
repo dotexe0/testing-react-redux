3ba4a3a338d38a82a0a85e2a71239baf7e5a871d
Daniel Varela <dvarela@wiser.com>
2018-11-19 08:57:03 -0800

    add fetch capability to commentbox

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import reduxPromise from 'redux-promise'
 import reducers from 'reducers'
 
 export default ({ children, initialState = {} }) => {
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(reduxPromise) // 3rd arg applies middlewares (this one is for crud)
+  )
   return (
-    <Provider store={createStore(reducers, initialState )}>
+    <Provider store={store}>
       {children}
     </Provider>
   )
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,18 @@
-import { SAVE_COMMENT } from 'actions/types'
+import axios from 'axios'
+import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types'
 
 export function saveComment(comment) {
   return {
     type: SAVE_COMMENT,
     payload: comment
+  }
+}
+
+export async function fetchComments() {
+  const res = await axios.get('http://jsonplaceholder.typicode.com/comments')
+  console.log('res', res)
+  return {
+    type: FETCH_COMMENTS,
+    payload: res
   }
 }
