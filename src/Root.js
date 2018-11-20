9eb18073e4635cd11ceb899ccf0d3415be995640
Daniel Varela <dvarela@wiser.com>
2018-11-20 07:30:05 -0800

    async middleware for handling api calls to outside endpoints

--- src/Root.js
+++ src/Root.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import reduxPromise from 'redux-promise'
+import async from 'middlewares/async'
 import reducers from 'reducers'
 
 export default ({ children, initialState = {} }) => {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(reduxPromise) // 3rd arg applies middlewares (this one is for crud)
+    applyMiddleware(async) // 3rd arg applies middlewares (this one is for crud)
   )
   return (
     <Provider store={store}>
